e53a39c2803d36991bbe29a4cd02d164133c964a
najahto <tahaomarnajah@gmail.com>
2021-11-17 00:17:52 +0100

    initial commit

---
+++ backend/src/test/java/com/najah/dev/reddit_clone_backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.najah.dev.reddit_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
